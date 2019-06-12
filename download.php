d636cf66048ced859b2b80e966a89be5bc8f31f4
wangchao <wangchao@wangchaos-iMac.lan>
2019-06-12 19:06:42 +0800

    fix

--- download.php
+++ download.php
@@ -2,10 +2,10 @@
 
 //执行脚本前先安装这两个依赖： sudo apt install php-xml php-mbstring
 
-//这里以下载小说 ‘http://www.xx31xs.org/13/13590/’ 为例子，原理很简单就是先下载目录页，然后下载每个章节内容，最后保存为txt文件
+//这里以下载小说 ‘http://www.xx31xs.org/134/134314/’ 为例子，原理很简单就是先下载目录页，然后下载每个章节内容，最后保存为txt文件
 
 $host = "http://www.xx31xs.org";
-$toc = file_get_contents($host . '/13/13590/');
+$toc = file_get_contents($host . '/134/134314/');
 
 $toc = iconv('GBK', 'UTF-8', $toc);//把gbk编码转换为utf8
 $tocDomDoc = new DOMDocument();
@@ -44,7 +44,11 @@ for ($i = 0; $i < $ddItems->length; $i++) {
     $chapterHtml = mb_convert_encoding($chapterHtml, "HTML-ENTITIES", "UTF-8");
     $chapterDomDoc->loadHTML($chapterHtml);
 
-    $novelContent .= $chapterDomDoc->textContent;
+    $chapterContent = $chapterDomDoc->textContent;
+    $startPos = mb_strpos($chapterContent, "下一章");
+    $chapterContent = mb_substr($chapterContent, $startPos + 3);
+
+    $novelContent .= "\r\n\r\n" . $chapterTitle . "\r\n\r\n" . $chapterContent;
 }
 
 file_put_contents($novelName.".txt", $novelContent);
